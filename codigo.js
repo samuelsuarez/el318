cd539dffccc52df9a1be2d72c98a3488b3d229a1
samuelsuarez <31040430+samuelsuarez@users.noreply.github.com>
2020-06-17 00:08:39 -0400

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-var opciones = ["Piedra","Papel","Tijera","Lagarto","Spock"];
+var opciones = ["Piedra","Papel","Tijera"];
 
 
 function confirmaJuego(){
@@ -12,14 +12,14 @@ function confirmaJuego(){
 }
 
 function iniciarJuego(){
-	var opcionUsuario=prompt("Elige una opcion entre <0-4>\n0: Piedra\n1: Papel\n2: Tijera\n3: Lagarto\n4: Spock\n");
+	var opcionUsuario=prompt("Elige una opcion entre <0-2>\n0: Piedra\n1: Papel\n2: Tijera");
 	var opcionUsuario=parseInt(opcionUsuario);
 	if(isNaN(opcionUsuario)){
 		alert("El valor ingresado no es numero...");
-	}else if(opcionUsuario<0 || opcionUsuario>4){
+	}else if(opcionUsuario<0 || opcionUsuario>2){
 		alert("El numero ingresado no corresponde a una de las opciones validas.");
 	}else{
-		var opcionJavascript=aleatorioJavascript(0,4);
+		var opcionJavascript=aleatorioJavascript(0,2);
 		if (compite(opcionJavascript,opcionUsuario)){
 			alert("Felicitaciones... Acaba de ganar");
 		}else{
@@ -36,27 +36,16 @@ function aleatorioJavascript(minimo,maximo){
 
 function compite(opcionUsuario,opcionJavascript){
 	alert("Usuario: "+opciones[opcionUsuario] + "    VS    "+"Javascript: " + opciones[opcionJavascript]);
-
-	if(opcionUsuario==opcionJavascript){
-		alert("Empataron!!!...");
-	}else if (opciones[opcionUsuario]=="Piedra"){
-		if(opciones[opcionJavascript]=="Tijera" || opciones[opcionJavascript]=="Lagarto"){
+        if (opciones[opcionUsuario]=="Piedra"){
+		if(opciones[opcionJavascript]=="Tijera"){
 			var ganador=true;
 		}
 	}else if (opciones[opcionUsuario]=="Papel"){
-		if(opciones[opcionJavascript]=="Piedra" || opciones[opcionJavascript]=="Spock"){
+		if(opciones[opcionJavascript]=="Piedra"){
 			var ganador=true;
 		}
 	}else if (opciones[opcionUsuario]=="Tijera"){
-		if(opciones[opcionJavascript]=="Papel" || opciones[opcionJavascript]=="Lagarto"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Lagarto"){
-		if(opciones[opcionJavascript]=="Papel" || opciones[opcionJavascript]=="Spock"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Spock"){
-		if(opciones[opcionJavascript]=="Piedra" || opciones[opcionJavascript]=="Tijera"){
+		if(opciones[opcionJavascript]=="Papel"){
 			var ganador=true;
 		}
 	}
